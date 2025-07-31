6346ea02f8a6a85f8319df452631ba56c427319f
mighty-baseplate <thisisjn.atharva@gmail.com>
2025-07-31 14:39:27 +0530

    new

--- components/sections/TaskManager.tsx
+++ components/sections/TaskManager.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { motion } from 'framer-motion';
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useApp, Task } from '@/components/providers/AppProvider'
